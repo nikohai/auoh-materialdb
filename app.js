6ae8d72a3ae0f2cde98852104222fbafce3715fe
nikohai <niko.haivala@seamk.fi>
2020-04-03 20:34:26 +0300

    commit

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 // npm install express
 // npm install init
 // npm istall nodemon
+// npm install mongoose
 // npm run start-dev
 
 const express = require('express');
 const port = process.env.PORT || 8080;
+const mongoose = require('mongoose');
 const app = express();
 
 const body_parser = require('body-parser');
 
-const material_controller = require('./material_controller')
+const material_controller = require('./material_controller');
 app.use(body_parser.json());  //req.body.name
 
 app.use(body_parser.urlencoded({
@@ -25,6 +27,7 @@ app.use(  (req, res, next)=>{
 // CRUD OPERATIONS
 
 //CREATE
+app.post("/api/material", material_controller.api_post_material);
 
 
 //READ
@@ -34,4 +37,18 @@ app.get("/api/materials", material_controller.api_get_materials);
 
 //DELETE
 
-app.listen(port);
+const database_uri = "mongodb+srv://server:QHRvDRhu4jK318zz@cluster0-tuqpv.mongodb.net/materialdb?retryWrites=true&w=majority";
+
+mongoose.connect(database_uri, {
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+}).then(()=>{
+    console.log('database connected');
+    app.listen(port);
+}).catch((err => {
+    console.log(err);
+}));
+// QHRvDRhu4jK318zz
+
--- material_controller.js
+++ material_controller.js
@@ -1,13 +1,53 @@
-
+const material_model = require('./material_model');
 
 
 // CREATE
 
+const api_post_material = (req, res, next) => {
+    console.log('api_post_material');
+    let data = {
+        name: req.body.name,
+        min_density: req.body.min_density,
+        max_density: req.body.max_density,
+        min_strength: req.body.min_strength,
+        max_strength: req.body.max_strength,
+        min_strength_density: req.body.min_strength / req.body.min_density,
+        max_strength_density: req.body.max_strength / req.body.max_density
+    };
+
+    let new_material = material_model(data);
+
+    new_material.save().then(() => {
+        console.log(new_material);
+        res.send(JSON.stringify(data));
+
+    }).catch(err => {
+        res.status(500);
+        res.send(err.errmsg);
+        console.log(err);
+    });
+
+
+    console.log(data);
+    
+};
 
 // READ
 
 const api_get_materials = (req, res, next) => {
-    res.send(JSON.stringify([]));
+    console.log('api_get_materials');
+
+    material_model.find({})
+    .lean()
+    .then(materials => {
+        res.send(JSON.stringify([materials]));
+    }).catch(err => {
+        res.status(500);
+        res.send(err.errmsg);
+        console.log(err); 
+    });
+
+    
 };
 
 
@@ -18,4 +58,5 @@ const api_get_materials = (req, res, next) => {
 
 
 // EXPORTS
+module.exports.api_post_material = api_post_material;
 module.exports.api_get_materials = api_get_materials;
